9835f9be33577a849e89d57d2f9d072974db8351
hfkhan13 <hfkhan13@gmail.com>
2022-05-25 16:19:13 +0800

    Module 2
    Category 1b

--- Code/productioncode.py
+++ Code/productioncode.py
@@ -9,3 +9,16 @@ def convert_to_lower(string):
     return string
 
 print(convert_to_lower("I Am Huzaifa"))  #test category 1a
+
+def contains_numeric(string):
+    number_in_string = False
+    for letter in string:
+        if letter.isdigit() == True:
+            number_in_string = True
+    if number_in_string == True:
+        print("There are numeric values in this string")
+    else:
+        print("There are no numeric values in this string")
+    
+contains_numeric("I am 20 years old") #test category 1b
+contains_numeric("I am twenty years old") #test category 1b
