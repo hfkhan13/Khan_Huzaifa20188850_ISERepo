79d2d7903653592a79e92bd169e87d7019001776
hfkhan13 <hfkhan13@gmail.com>
2022-05-25 15:14:54 +0800

    Module 1
    Completed Category 1a

--- Code/productioncode.py
+++ Code/productioncode.py
@@ -3,3 +3,9 @@ def convert_to_upper(string):
     return string
 
 print(convert_to_upper("i am huzaifa"))  #test category 1a
+
+def convert_to_lower(string):
+    string = string.lower()
+    return string
+
+print(convert_to_lower("I Am Huzaifa"))  #test category 1a
